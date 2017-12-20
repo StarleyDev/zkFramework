e73bbe3e7ae56291c6f45505a3359861eaffaea6
Starlley Cazorla <stdev@outlook.com.br>
2017-12-20 00:32:03 -0200

    Modificado Interface

--- src/br/com/st/controler/Cindex.java
+++ src/br/com/st/controler/Cindex.java
@@ -21,7 +21,13 @@ public class Cindex extends SelectorComposer<Component> {
 
 	// Aqui ele ira vincular o botão com a textbox //
 	@Wire
-	private Textbox txtbxMaiuscula;
+	private Textbox txtbxAgencia;
+	@Wire
+	private Textbox txtbxConta;
+	@Wire
+	private Textbox txtbxTitular;
+	@Wire
+	private Textbox txtbxSaldo;
 
 	@Wire
 	private Listbox lstbxOperacoesRealizadas;
@@ -29,40 +35,41 @@ public class Cindex extends SelectorComposer<Component> {
 	private ArrayList<LinkedHashMap<String, String>> listaOperacoes = new ArrayList<LinkedHashMap<String, String>>();
 
 	// Metodo que ira ser acionado ao clicar no btnMaiuscula //
-	@Listen("onClick = #btnMaiuscula")
-	public void trocarPorMaiuscula() {
-		String antes = this.txtbxMaiuscula.getValue(); // Aqui ele pega o valor
-														// inserido
-		String depois = antes.toUpperCase(); // Aqui ele modifica o valor
-		this.txtbxMaiuscula.setValue(depois); // Aqui ele seta o valor
-												// modificado
+	@Listen("onClick = #btnCad")
+	public void addCadastro() {
+		String ag = this.txtbxAgencia.getValue();
+		String cc = this.txtbxConta.getValue();
+		String tt = this.txtbxTitular.getValue();
+		String sd = this.txtbxSaldo.getValue();
 
 		LinkedHashMap<String, String> hash = new LinkedHashMap<String, String>();
-		hash.put("operacoes", "Maiúscula");
-		hash.put("antes", antes); // Aqui ele mostra o valor anterior
-		hash.put("depois", depois); // O valor modificado
+		hash.put("agencia", ag);
+		hash.put("conta", cc); // Aqui ele mostra o valor anterior
+		hash.put("titular", tt);
+		hash.put("saldo", sd);
 		listaOperacoes.add(hash); // Adiciona a lista
 
 		this.lstbxOperacoesRealizadas.setModel(new ListModelArray<LinkedHashMap<String, String>>(this.listaOperacoes));
 	}
 
-	// Aqui ele ira vincular o botão com a textbox //
-	@Wire
-	private Textbox txtbxMinusculo;
-
-	@Listen("onClick = #btnMinusculo")
-	public void trocarPorMinuscula() {
-		String antes = this.txtbxMinusculo.getValue();
-		String depois = antes.toLowerCase();
-		this.txtbxMinusculo.setValue(depois);
-
-		LinkedHashMap<String, String> hash = new LinkedHashMap<String, String>();
-		hash.put("operacoes", "Minúscula");
-		hash.put("antes", antes);
-		hash.put("depois", depois);
-		listaOperacoes.add(hash);
-
-		this.lstbxOperacoesRealizadas.setModel(new ListModelArray<LinkedHashMap<String, String>>(this.listaOperacoes));
-	}
+	// // Aqui ele ira vincular o botão com a textbox //
+	// @Wire
+	// private Textbox txtbxMinusculo;
+	//
+	// @Listen("onClick = #btnMinusculo")
+	// public void trocarPorMinuscula() {
+	// String antes = this.txtbxMinusculo.getValue();
+	// String depois = antes.toLowerCase();
+	// this.txtbxMinusculo.setValue(depois);
+	//
+	// LinkedHashMap<String, String> hash = new LinkedHashMap<String, String>();
+	// hash.put("operacoes", "Minúscula");
+	// hash.put("antes", antes);
+	// hash.put("depois", depois);
+	// listaOperacoes.add(hash);
+	//
+	// this.lstbxOperacoesRealizadas.setModel(new
+	// ListModelArray<LinkedHashMap<String, String>>(this.listaOperacoes));
+	// }
 
 }
